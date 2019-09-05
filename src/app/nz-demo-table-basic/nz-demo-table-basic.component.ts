d82ca73481cc6425ebfbe2450ce4ddf80b13bd89
begin256 <mec29011@163.com>
2019-09-05 23:30:22 +0800

    init

---
+++ src/app/nz-demo-table-basic/nz-demo-table-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nz-demo-table-basic',
+  templateUrl: './nz-demo-table-basic.component.html',
+  styleUrls: ['./nz-demo-table-basic.component.css']
+})
+export class NzDemoTableBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
